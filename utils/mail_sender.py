c6b8a05901e341b6fe4a9be13e721dd7ba976fdf
Andersson Camilo Cardenas Guarin <anderssonccg@gmail.com>
2025-04-02 13:14:24 -0500

    feat: add email template using Jinja library

--- utils/mail_sender.py
+++ utils/mail_sender.py
@@ -1,5 +1,6 @@
 import os
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from jinja2 import Environment, FileSystemLoader
 
 MAIL_USERNAME = os.getenv("MAIL_USERNAME")
 MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
@@ -22,10 +23,15 @@ conf = ConnectionConfig(
 async def send_verification_email(email: str, token: str):
     token = token["access_token"]
     verification_url = f"{DOMAIN}/users/verify-email?token={token}"
+
+    env = Environment(loader=FileSystemLoader("templates"))
+    template = env.get_template("email_verification.html")
+    html_content = template.render(verification_url=verification_url)
+    
     message = MessageSchema(
         subject="Verifica tu correo",
         recipients=[email],
-        body=f"Confirma tu cuenta haciendo clic en este enlace: {verification_url}",
+        body=html_content,
         subtype="html",
     )
     fm = FastMail(conf)
